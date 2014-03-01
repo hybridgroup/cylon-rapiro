06aef4f8f2e7ef36cd14185d19310058e1317fc6
deadprogram <ron@hybridgroup.com>
2014-03-01 10:03:39 -0800

    A better walk example

--- examples/rapiro_walk.js
+++ examples/rapiro_walk.js
@@ -5,26 +5,26 @@ Cylon.robot({
   device: {name: 'rapiro', driver: 'rapiro'},
 
   work: function(my) {
-	  my.startWalking = function() {
-	  	Logger.info("forward");
-	  	my['doneWalking'] = false ;
+    my.startWalking = function() {
+      Logger.info("forward");
+      my['doneWalking'] = false ;
 
-	    every(1..second(), function() {
-	    	if (my['doneWalking'] == false)
-	    		my.rapiro.forward();
-	    });
-	  }
+      every(1..second(), function() {
+        if (my['doneWalking'] == false)
+          my.rapiro.forward();
+      });
+    }
 
-	  my.stopWalking = function() {
-	  	Logger.info("halt");
-	    my.rapiro.halt();
-	    my['doneWalking'] = true;
-	  }
+    my.stopWalking = function() {
+      Logger.info("halt");
+      my.rapiro.halt();
+      my['doneWalking'] = true;
+    }
 
-  	my.rapiro.on('start', function() {
-  		my.startWalking();
-	  
-	    after(10..seconds(), my.stopWalking);
-	  });
+    my.rapiro.on('start', function() {
+      my.startWalking();
+    
+      after(10..seconds(), my.stopWalking);
+    });
   }
 }).start();
